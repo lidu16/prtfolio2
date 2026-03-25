915f2e9fb84a405d1aedf06076b4a0add60364af
lidu16 <lidiyamesenbet16@gmail.com>
2026-03-25 20:40:34 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css';
+import { Hero } from './components/Hero';
+import { Projects } from './components/Projects';
+import { contact } from './components/Contact';
+import  {Navbar}  from './components/Navbar';
+
+function App() {
+ 
+
+  return <>
+  <Navbar/>
+  <Hero/>
+  <Projects/>
+  <contact/>  
+  </>;
+}
+  
+      
+  
+
+
+export default App
